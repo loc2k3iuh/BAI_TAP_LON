e82bd121e98ad5ff03d8d93ba389fe5eddae763d
PhamHuuThuan <92664025+PhamHuuThuan@users.noreply.github.com>
2024-11-27 16:40:23 +0700

    add JWT Token Filter

--- BackEnd_Shop_Phu_Kien/shopapp/src/main/java/com/project/shopapp/filters/JwtTokenFilter.java
+++ BackEnd_Shop_Phu_Kien/shopapp/src/main/java/com/project/shopapp/filters/JwtTokenFilter.java
@@ -1,7 +1,82 @@
 package com.project.shopapp.filters;
+import com.project.shopapp.components.JwtTokenUtils;
+import com.project.shopapp.models.User;
+import lombok.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.util.Pair;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Value("${api.prefix}")
+    private String apiPrefix;
+
+    private final UserDetailsService userDetailsService;
+    private final JwtTokenUtils jwtTokenUtil;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)throws ServletException, IOException {
+
+        try{
+            if (isByPassToken(request)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            final String authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                return;
+            }
+            final String token = authorizationHeader.substring(7);
+            final String phoneNumber = jwtTokenUtil.extractPhoneNumber(token);
+            if (phoneNumber != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                User userDetails = (User) userDetailsService.loadUserByUsername(phoneNumber);
+                if (jwtTokenUtil.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        }
+    }
 
 
-
-public class JwtTokenFilter extends OncePerRequestFilter{
-  
+    private boolean isByPassToken(@NonNull HttpServletRequest request){
+        final List<Pair<String, String>> bypassTokens = Arrays.asList(
+                Pair.of(String.format("%s/products", apiPrefix), "GET"),
+                Pair.of(String.format("%s/products", apiPrefix), "POST"),
+                Pair.of(String.format("%s/categories", apiPrefix), "GET"),
+                Pair.of(String.format("%s/users/register", apiPrefix), "POST"),
+                Pair.of(String.format("%s/users/login", apiPrefix), "POST"),
+                Pair.of(String.format("%s/roles", apiPrefix), "GET"),
+                Pair.of(String.format("%s/products/uploads", apiPrefix), "POST")
+        );
+        for(Pair<String, String> bypassToken : bypassTokens){
+            if(request.getServletPath().contains(bypassToken.getFirst()) && request.getMethod().equals(bypassToken.getSecond())){
+                return true;
+            }
+        }
+        return false;
+    }
 }
