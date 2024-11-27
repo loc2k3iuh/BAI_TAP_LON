725a1615df6396527cfb0ee495964958b8f950e9
Loc Nguyen <nguyentanloc12c9tlt@gmail.com>
2024-11-27 16:30:47 +0700

    fix

--- BackEnd_Shop_Phu_Kien/shopapp/src/main/java/com/project/shopapp/controllers/OrderDetailController.java
+++ BackEnd_Shop_Phu_Kien/shopapp/src/main/java/com/project/shopapp/controllers/OrderDetailController.java
@@ -1,15 +1,5 @@
 package com.project.shopapp.controllers;
 
-import com.project.shopapp.components.LocalizationUtils;
-import com.project.shopapp.dtos.requests.order.OrderDetailDTO;
-import com.project.shopapp.dtos.responses.orderdetail.OrderDetailResponse;
-import com.project.shopapp.services.orderdetail.OrderDetailService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 
 public class OrderDetailController {
