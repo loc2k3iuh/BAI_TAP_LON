80931db77f8607cdd3eb0ac0997032671f1725cb
hoanghuytoi <hoanghuytoi03@Gmail.com>
2024-12-01 15:59:07 +0700

    ProductResponse

--- BackEnd_Shop_Phu_Kien/shopapp/src/main/java/com/project/shopapp/dtos/responses/product/ProductResponse.java
+++ BackEnd_Shop_Phu_Kien/shopapp/src/main/java/com/project/shopapp/dtos/responses/product/ProductResponse.java
@@ -1,7 +1,48 @@
 package com.project.shopapp.dtos.responses.product;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.shopapp.dtos.responses.BaseResponse;
+import com.project.shopapp.models.Product;
+import com.project.shopapp.models.ProductImage;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductResponse extends BaseResponse {
 
-   
+    int id;
+    String name;
+    Float price;
+    String description;
+    String thumbnail;
+    @JsonProperty("category_id")
+    int categoryId;
+
+    @JsonProperty("product_images")
+    List<ProductImage> productImages = new ArrayList<>();
+
+
+    public static ProductResponse fromProduct(Product product) {
+        ProductResponse productResponse=  ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .thumbnail(product.getThumbnail())
+                .productImages(product.getProductImages())
+                .categoryId(product.getCategory().getId())
+                .build();
+        productResponse.setCreatedAt(product.getCreatedAt());
+        productResponse.setUpdatedAt(product.getUpdatedAt());
+        return productResponse;
+    }
 }
